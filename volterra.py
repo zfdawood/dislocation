cbd30eedd8c5a232a8bf541c000f7506252a58ae
zfdawood <89550184+zfdawood@users.noreply.github.com>
2025-12-18 14:06:31 -0500

    something definitive

--- volterra.py
+++ volterra.py
@@ -18,24 +18,226 @@ def _():
 
     rng = np.random.default_rng()
     matplotlib.rcParams['animation.embed_limit'] = float('inf')
-    return mo, np, plt, rng
+    return mo, np, plt, rng, solve_ivp
+
+
+@app.cell
+def _():
+    # # constants
+    # N = 400 # number of edge dislocations, must be even 
+    # pi = np.pi # adding the "np" every time honestly adds up 
+    # sigma_ext = 0 # external shear stress 
+    # b = 2.56e-10 #4e-8 # burgers vector magnitude for copper use 2.56 A
+    # chi_d = 1e-6 #1e-12 # effective mobility, for ice ~1e-12, for copper ~1e-6, see https://journals.aps.org/prb/abstract/10.1103/PhysRevB.8.3537
+    # mu = 44e9 # shear modulus (same units as sheer stress), for ice, use 3 GPa, for copper use 44 GPa
+    # nu = 0.34 # poisson ratio, for ice use 0.3, for copper use 0.34
+    # ye = 1.6e-9 # burgers vector annihilation distance. For copper use 1.6 nm 
+    # D = mu / (2 * np.pi * (1 - nu))
+    # L = 100 * ye # size of cell
+    # t0 = ye**2 / (chi_d * D * b**3)
+    # LATTICE_EDGE_CT = int(L / b) # 30
+    return
 
 
 @app.cell
 def _(np):
-    # constants
-    N = 400 # number of edge dislocations, must be even 
-    pi = np.pi # adding the "np" every time honestly adds up 
-    sigma_ext = 0 # external shear stress 
-    b = 2.56e-10 #4e-8 # burgers vector magnitude for copper use 2.56 A
-    chi_d = 1e-12 # effective mobility, try something like 1 \mu m /sMPa
-    mu = 44e9 # shear modulus (same units as sheer stress), for ice, use 3 GPa, for copper use 44 GPa
+    # scaled constants
+    N = 40
+    pi = np.pi 
+    sigma_ext = 1
+    b = 1 
+    chi_d = 1
+    mu = 1
     nu = 0.34 # poisson ratio, for ice use 0.3, for copper use 0.34
-    ye = 1.6e-9 # burgers vector annihilation distance. For copper use 1.6 nm 
+    ye = 1 
     D = mu / (2 * np.pi * (1 - nu))
     L = 100 * ye # size of cell
+    t0 = ye**2 / (chi_d * D * b**3)
     LATTICE_EDGE_CT = int(L / b) # 30
-    return D, L, LATTICE_EDGE_CT, N, b, chi_d, nu, pi, sigma_ext
+    return D, L, LATTICE_EDGE_CT, N, b, chi_d, nu, pi, t0, ye
+
+
+@app.cell
+def _(L, N, b, np, rng):
+    dislocations = np.column_stack((
+        rng.random(N) * L, # xs
+        # (rng.random(N) * 1e-9) + 1e-7, 
+        rng.random(N) * L, # ys
+        # b vector directions, making sure the number of positive bs == number of negative bs
+        b * rng.permuted(
+            np.concatenate((
+                np.ones(int(N/2)),
+                 -(np.ones(int(N/2)))
+            ))
+        )
+    ))
+
+    # dislocations = np.array([
+    #     [4*b, 0, -b],
+    #     [2*b, 0, b]
+    # ])
+
+    return (dislocations,)
+
+
+@app.cell
+def _(apply_dislocations, b, dislocations, mo, plot_lattice):
+    X, Y = apply_dislocations(dislocations, b)
+    mo.mpl.interactive(plot_lattice(X, Y, dislocations))
+    return
+
+
+@app.cell
+def _(dislocations, sigma_s_analytic_sum, v_i):
+    # velocities are extremely small, even with the greater effectvie mobility of copper 
+    v_i(sigma_s_analytic_sum, dislocations[:,0], dislocations[0,0], dislocations, 0)
+    return
+
+
+@app.cell
+def _(L, N, dislocations, np, sigma_s_analytic_sum, solve_ivp, t0, v_i, ye):
+    # we use this vector to make sure that canceled dislocations don't contribute to dxdt 
+    elimination_vector = np.ones(N)
+
+    # number of points to use in the ivp solver
+    num_tpoints = 1000
+
+
+    def dxsdt_func(t, xs_p, sigma_ext):
+        # the positions used by the ivp solver aren't bound to the box so we apply that binding here
+        xs = xs_p % L
+
+        # # annihilation_list is a list of ordered pairs of annihilated dislocations 
+        # # we mark a dislocation to be annihilated when... 
+        # annihilation_list = zip(*(np.nonzero(
+        #     # ... two dislocations are within ye of each other... 
+        #     # (this particular implementation draws more of a square bounding box 
+        #     # than a circle one to check closeness but it's fine)
+        #     (np.isclose(xs.reshape(-1,1), xs.reshape(1,-1), rtol=0, atol=ye))
+            # & (np.isclose(dislocations[:,1].reshape(-1,1), dislocations[:,1].reshape(1,-1), rtol=0, atol=ye))
+        #     # ... and the dislocations have opposite burgers vectors... 
+        #     & (dislocations[:,2].reshape(-1,1) != dislocations[:,2].reshape(1,-1)) 
+        #     # ... and they aren't the same dislocation (should be taken care of by the previous point)
+        #     # & (np.logical_not(np.diagflat(np.repeat(True, N))))
+        # )))
+    
+        # for (i, j) in annihilation_list: 
+        #     if elimination_vector[i] != 0 or elimination_vector[j] != 0:
+        #         print(f"annihilation event with {i} and {j} at t = {t}, the {t/t0}th step")
+        #     elimination_vector[i] = 0
+        #     elimination_vector[j] = 0
+        # # this whole thing can probably be improved by concatenating after np.nonzero and then
+        # # just elimination_vector[concatenated id'd close dislocations, duplicates are fine] = 0
+    
+    
+        # this is really slow but should be fine for low N - outer product strategy as above would be faster
+        for i in range(0,N):
+            for j in range(0,N):
+                if (np.linalg.norm(np.array([xs[i], dislocations[i,1]]) - 
+                                   np.array([xs[j], dislocations[j,1]])) < ye) & (
+                dislocations[i,2] != dislocations[j,2]):               
+                    if (elimination_vector[i] != 0) or (elimination_vector[j] != 0):
+                        print(f"annihilation event with {i}: {xs[i]}, {dislocations[i,1]} and {j}: {xs[j]}, {dislocations[j,1]} at t = {t}, the {t/t0}th step")
+                    
+                        elimination_vector[i] = elimination_vector[j] = 0
+
+            
+
+        dxsdt = np.zeros(N)
+        for i, x in enumerate(xs):
+            dxsdt[i] = elimination_vector[i] * v_i(sigma_s_analytic_sum, xs, x, dislocations, i, sigma_ext)
+        return dxsdt
+
+    # keep this function call with the dxsdt func definition and elimination_vector init
+    ivp_result = solve_ivp(dxsdt_func, (t0, num_tpoints*t0), dislocations[:,0], 
+                           t_eval=np.linspace(t0, num_tpoints * t0, num_tpoints), method="RK23", args=(0,))
+    return dxsdt_func, ivp_result, num_tpoints
+
+
+@app.cell
+def _(L, ivp_result, np):
+    print(ivp_result.y[np.array([18,28]),:] % L)
+    return
+
+
+@app.cell
+def _():
+    # sloppy code to double-check closeness 
+    # lastxs = ivp_result.y[:,-1]
+    # lastys = dislocations[:,1]
+    # for i in range(0,N):
+    #     for j in range(0,N):
+    #         if (np.sqrt((lastxs[i] - lastxs[j])**2 + (lastys[i] - lastys[j])**2) < ye) and dislocations[i,2] != dislocations[j,2] and (elimination_vector[i] != 0 or elimination_vector[j] != 0):
+    #             print(f"{i} and {j} and {abs(ivp_result.y[:,-1][i] - ivp_result.y[:,-1][j])} and {abs(lastys[i]-lastys[j])}")
+    return
+
+
+@app.cell
+def _(
+    L,
+    apply_dislocations,
+    b,
+    dislocations,
+    ivp_result,
+    mo,
+    np,
+    plot_lattice,
+):
+    post_relaxation_dislocations = np.copy(dislocations)
+    post_relaxation_dislocations[:,0] = ivp_result.y[:,-1] % L 
+
+    X_post, Y_post = apply_dislocations(post_relaxation_dislocations, b)
+    mo.mpl.interactive(plot_lattice(X_post, Y_post, post_relaxation_dislocations))
+    return (post_relaxation_dislocations,)
+
+
+@app.cell
+def _(
+    dxsdt_func,
+    np,
+    num_tpoints,
+    post_relaxation_dislocations,
+    solve_ivp,
+    t0,
+):
+    ivp_result_ext_stress = solve_ivp(dxsdt_func, (t0, num_tpoints*t0), post_relaxation_dislocations[:,0], 
+                           t_eval=np.linspace(t0, num_tpoints * t0, num_tpoints), method="RK23", args=(0.035,))
+    return (ivp_result_ext_stress,)
+
+
+@app.cell
+def _(N, dislocations, ivp_result_ext_stress, np, sigma_s_analytic_sum, v_i):
+    # rigorify this 
+    strain_deriv = np.zeros(800)
+
+    old_elimination = np.ones(N)
+    old_elimination[[18, 28]] = 0 
+
+    new_elimination = np.ones(N)
+    new_elimination[[18, 28, 38, 39]] = 0
+
+    for i in range(800):
+        for j in range(N):
+            temp_dislocations = np.copy(dislocations)
+            temp_dislocations[:,0] = ivp_result_ext_stress.y[:,i]
+            if (i < 448) & (j in [18,28]) :
+                continue 
+            elif (i > 448) & (j in [18, 28, 38, 39]):
+                continue
+            else:
+                # FIX THE vi and sigma fns to take current xs and not dislocations 
+                strain_deriv[i] += dislocations[j,2] * v_i(sigma_s_analytic_sum, ivp_result_ext_stress.y[:,i],
+                                             ivp_result_ext_stress.y[j,i], temp_dislocations, j, sigma_ext=0.035)
+    print(strain_deriv)
+    return (strain_deriv,)
+
+
+@app.cell
+def _(ivp_result_ext_stress, np, plt, strain_deriv):
+    fig, ax = plt.subplots()
+    ax.plot(np.log10(ivp_result_ext_stress.t[:600]), np.log10(strain_deriv[:600]))
+    plt.show()
+    return
 
 
 @app.cell
@@ -95,42 +297,21 @@ def _(L, LATTICE_EDGE_CT, calculate_displacements, np):
 
 @app.cell
 def _(plt):
-    def plot_lattice(x, y, dislocations):
+    def plot_lattice(lattice_x, lattice_y, dislocations):
         fig, ax = plt.subplots(figsize=(9, 9))
 
-        ax.scatter(x, y)
+        ax.scatter(lattice_x, lattice_y)
 
-        ax.scatter(dislocations[:,0], dislocations[:,1], c=dislocations[:,2], label="dislocation lines")
+        ax.scatter(dislocations[:,0], dislocations[:,1], c=dislocations[:,2], label="dislocation centers")
 
+        for i, xy in enumerate(zip(dislocations[:,0], dislocations[:,1])):
+            ax.annotate(i, xy, c='r')
+    
         ax.axis('equal')
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         return fig
     return (plot_lattice,)
-
-
-@app.cell
-def _(L, N, apply_dislocations, b, mo, np, plot_lattice, rng):
-    dislocations = np.column_stack((
-        rng.random(N) * L, # xs
-        rng.random(N) * L, # ys
-        # b vector directions, making sure the number of positive bs == number of negative bs
-        b * rng.permuted(
-            np.concatenate((
-                np.ones(int(N/2)),
-                 -(np.ones(int(N/2)))
-            ))
-        )
-    ))
-
-    # dislocations = np.array([
-    #     [4*b, 0, -1],
-    #     [2*b, 0, 1]
-    # ])
-
-    X, Y = apply_dislocations(dislocations, b)
-    mo.mpl.interactive(plot_lattice(X, Y, dislocations))
-    return (dislocations,)
 
 
 @app.cell
@@ -155,36 +336,13 @@ def _(D, L, N, b, np, pi):
 
 
 @app.cell
-def _(dislocations, sigma_s_analytic_sum):
-    # probably worth pointing out just how big these stresses are (order of 1e6)
-    sigma_s_analytic_sum(dislocations[0,0], dislocations[0,1], dislocations)
-    return
-
-
-@app.cell
-def _(b, chi_d, sigma_ext):
+def _(b, chi_d):
     # eqn 2 in the paper
-    def v_i(sigma_fn, xs, curr_x, dislocations, dislocation_i):
+    def v_i(sigma_fn, xs, curr_x, dislocations, dislocation_i, sigma_ext=0):
         _, curr_y, curr_b = dislocations[dislocation_i,:]
         bracketed = dislocations[:,2] @ sigma_fn(curr_x, curr_y, dislocations) + sigma_ext 
-        print(bracketed)
-        print(curr_b)
-        print(curr_b * b**2 * chi_d)
-        return curr_b * b**2 * chi_d * bracketed
+        return curr_b * b * chi_d * bracketed
     return (v_i,)
-
-
-@app.cell
-def _(dislocations, sigma_s_analytic_sum, v_i):
-    # but now i feel like this has gone too far in the other direction, size-wise (order of 1e-23)
-    v_i(sigma_s_analytic_sum, dislocations[:,0], dislocations[0,0], dislocations, 0)
-    return
-
-
-@app.cell
-def _(b, chi_d):
-    print(b**3 * chi_d * 1e7)
-    return
 
 
 if __name__ == "__main__":
