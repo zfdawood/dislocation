f2a2381d2bda6ee90dbc0a5000a827c46fd08a64
Zia Dawood <dawood.zachariah@gmail.com>
2026-01-12 22:32:49 -0500

    testing and fixing some bugs in the solveivp call

--- dimensionless.py
+++ dimensionless.py
@@ -36,57 +36,6 @@ def _(dislocations, vs):
 
 
 @app.cell
-def _(L, N, dislocations, t0, vs, ye):
-    # we use this vector to make sure that canceled dislocations don't contribute to dxdt 
-    elimination_vector = np.column_stack((np.ones(N), np.zeros(N)))
-
-    # number of points to use in the ivp solver
-    num_tpoints = 1000
-
-
-    def dxsdt_func(t, xs_p, sigma_ext):
-        # the positions used by the ivp solver aren't bound to the box so we apply that binding here
-        xs = xs_p % L
-        print(t)
-
-        # annihilation_list is a list of ordered pairs of annihilated dislocations 
-        # we mark a dislocation to be annihilated when... 
-        annihilation_list = np.column_stack(np.nonzero(
-            # ... two dislocations are within ye of each other... 
-            # (this particular implementation draws more of a square bounding box 
-            # than a circle one to check closeness but it's fine)
-            (np.isclose(xs.reshape(-1,1), xs.reshape(1,-1), rtol=0, atol=ye))
-            & (np.isclose(dislocations[:,1].reshape(-1,1), dislocations[:,1].reshape(1,-1), rtol=0, atol=ye))
-            # ... and the dislocations have opposite burgers vectors... 
-            & (dislocations[:,2].reshape(-1,1) != dislocations[:,2].reshape(1,-1)) 
-            # ... and they haven't been previously selected...
-            # (this looks convoluted but the operation would take literally forever without it)
-            & np.outer((elimination_vector[:,0].reshape(-1,1)), (elimination_vector[:,0])).astype(bool)
-            # ... and they aren't the same dislocation (should be taken care of by the previous point)
-            # & (np.logical_not(np.diagflat(np.repeat(True, N))))
-        ))
-
-        for (i, j) in annihilation_list: 
-            # if (elimination_vector[i,0] != 0) or (elimination_vector[j,0] != 0):
-            #     print(f"annihilation event with {i} and {j} at t = {t}, the {t/t0}th step")
-            if elimination_vector[i,0] != 0:
-                elimination_vector[i,:] = [0,t] 
-            if elimination_vector[j,0] != 0:
-                elimination_vector[j,:] = [0,t]
-
-        # this whole thing can probably be improved by concatenating after np.nonzero and then
-        # just elimination_vector[concatenated id'd close dislocations, duplicates are fine] = 0
-        # but that'll make recording the time difficult 
-
-        return elimination_vector[:,0] * vs(sigma_s_analytic_sum, xs, dislocations)
-
-    # keep this function call with the dxsdt func definition and elimination_vector init
-    ivp_result = solve_ivp(dxsdt_func, (t0, num_tpoints*t0), dislocations[:,0], 
-                           t_eval=np.linspace(t0, num_tpoints * t0, num_tpoints), method="RK45", args=(0,))
-    return dxsdt_func, elimination_vector, ivp_result, num_tpoints
-
-
-@app.cell
 def _():
     # with open("./pickles/1000iter_rk45_LAPTOP.pkl", "wb") as file:
     #     pickle.dump((dislocations, ivp_result, elimination_vector), file)
@@ -385,207 +334,202 @@ def sigma_s_analytic_sum(xs, ys, L):
         den = L**2 * (np.cos(x_arg) - np.cosh(y_arg))**2
 
         # sets stress to zero when X[i] or Y[i] = 0 (ie at the point we dont want to compute)
-        return -np.divide(num, den, out=np.zeros((xs.size,xs.size)), where=((X!=0) & (Y!=0))) # can i just do elim.multipliers != 0??? - maybe need to make sure it doesn't contribute to stress at all
+        return -np.divide(num, den, out=np.zeros((xs.size,xs.size)), where=((X!=0) & (Y!=0)))
 
 
-@app.cell
-def _(L, dislocations, vs, ye):
-    def dxsdt_func(t, xs_p, sigma_ext, elim, lc):
-        # solve_ivp results and inputs don't obey periodic conditions 
-        xs = xs_p % L
+@app.function
+def dxsdt_func(t, xs_p, sigma_ext, elim, lc):
+    # solve_ivp results and inputs don't obey periodic conditions 
+    xs = xs_p % lc.L
 
-        # annihilation_list is a list of ordered pairs of annihilated dislocations 
-        # we mark a dislocation to be annihilated when... 
-        annihilation_list = np.column_stack(np.nonzero(
-            # ... two dislocations are within ye of each other... 
-            # (this particular implementation draws more of a square bounding box 
-            # than a circle one to check closeness but it's fine)
-            (np.isclose(xs.reshape(-1,1), xs.reshape(1,-1), rtol=0, atol=ye))
-            & (np.isclose(lc.ys.reshape(-1,1), lc.ys.reshape(1,-1), rtol=0, atol=ye))
-            # ... and the dislocations have opposite burgers vectors... 
-            & (lc.bs.reshape(-1,1) != lc.bs.reshape(1,-1)) 
-            # ... and they haven't been previously selected...
-            # (this looks convoluted but the operation would take literally forever without it)
-            & np.outer((elim.ms.reshape(-1,1)), (elim.ms)).astype(bool)
-            # ... and they aren't the same dislocation (should be taken care of by the previous point)
-            # & (np.logical_not(np.diagflat(np.repeat(True, N))))
-        ))
+    # annihilation_list is a list of ordered pairs of annihilated dislocations 
+    # we mark a dislocation to be annihilated when... 
+    annihilation_list = np.column_stack(np.nonzero(
+        # ... two dislocations are within ye of each other... 
+        # (this particular implementation draws more of a square bounding box 
+        # than a circle one to check closeness but it's fine)
+        (np.isclose(xs.reshape(-1,1), xs.reshape(1,-1), rtol=0, atol=lc.ye))
+        & (np.isclose(lc.ys.reshape(-1,1), lc.ys.reshape(1,-1), rtol=0, atol=lc.ye))
+        # ... and the dislocations have opposite burgers vectors... 
+        & (lc.bs.reshape(-1,1) != lc.bs.reshape(1,-1)) 
+        # ... and they haven't been previously selected...
+        # (this looks convoluted but the operation would take literally forever without it)
+        & np.outer((elim.mults.reshape(-1,1)), (elim.mults)).astype(bool)
+        # ... and they aren't the same dislocation (should be taken care of by the previous point)
+        # & (np.logical_not(np.diagflat(np.repeat(True, N))))
+    ))
 
-        # # annihilation_list is a list of ordered pairs of annihilated dislocations 
-        # # we mark a dislocation to be annihilated when...
-        # annihilation_list = np.column_stack(np.nonzero(
-        #     # ...two dislocations are within ye of each other...
-        #     (np.sqrt((xs - xs.reshape(-1,1))**2 + (lc.ys - lc.ys.reshape(-1,1))**2) < lc.ye)
-        #     # ... and the dislocations have opposite burgers vectors... 
-        #     & (lc.bs.reshape(-1,1) != lc.bs.reshape(1,-1)) 
-        #     # ... and they haven't been previously selected...
-        #     # (this looks convoluted but the operation would take literally forever without it)
-        #     & np.outer((elim.multipliers.reshape(-1,1)), (elim.multipliers)).astype(bool)
-        #     # ... and they aren't the same dislocation (should be taken care of by the previous point)
-        #     # & (np.logical_not(np.diagflat(np.repeat(True, N))))
-        # ))
-    
-        for (i, j) in annihilation_list: 
-            # if (elimination_vector[i,0] != 0) or (elimination_vector[j,0] != 0):
-            #     print(f"annihilation event with {i} and {j} at t = {t}, the {t/t0}th step")
-            if elim.ms[i] != 0:
-                elim.ms[i] = 0 
-                elim.elim_times[i] = t 
-            if elim.ms[j] != 0:
-                elim.ms[j] = 0 
-                elim.elim_times[j] = t 
+    # # annihilation_list is a list of ordered pairs of annihilated dislocations 
+    # # we mark a dislocation to be annihilated when...
+    # annihilation_list = np.column_stack(np.nonzero(
+    #     # ...two dislocations are within ye of each other...
+    #     (np.sqrt((xs - xs.reshape(-1,1))**2 + (lc.ys - lc.ys.reshape(-1,1))**2) < lc.ye)
+    #     # ... and the dislocations have opposite burgers vectors... 
+    #     & (lc.bs.reshape(-1,1) != lc.bs.reshape(1,-1)) 
+    #     # ... and they haven't been previously selected...
+    #     # (this looks convoluted but the operation would take literally forever without it)
+    #     & np.outer((elim.multipliers.reshape(-1,1)), (elim.multipliers)).astype(bool)
+    #     # ... and they aren't the same dislocation (should be taken care of by the previous point)
+    #     # & (np.logical_not(np.diagflat(np.repeat(True, N))))
+    # ))
 
-        # this whole thing can probably be improved by concatenating after np.nonzero and then
-        # just elimination_vector[concatenated id'd close dislocations, duplicates are fine] = 0
-        # but that'll make recording the time difficult 
+    for (i, j) in annihilation_list: 
+        # if (elimination_vector[i,0] != 0) or (elimination_vector[j,0] != 0):
+        #     print(f"annihilation event with {i} and {j} at t = {t}, the {t/t0}th step")
+        if elim.mults[i] != 0:
+            elim.mults[i] = 0 
+            elim.elim_times[i] = t 
+        if elim.mults[j] != 0:
+            elim.mults[j] = 0 
+            elim.elim_times[j] = t 
 
-        return elim.ms * vs(sigma_s_analytic_sum, xs, dislocations)
-    
-    return (dxsdt_func,)
+    # this whole thing can probably be improved by concatenating after np.nonzero and then
+    # just elimination_vector[concatenated id'd close dislocations, duplicates are fine] = 0
+    # but that'll make recording the time difficult 
+
+    return elim.mults * lc.vs(sigma_s_analytic_sum, xs)
 
 
 @app.class_definition
 # class to help remove annihilated dislocations from the system 
 class Eliminator:
     def __init__(self, size):
-        # values to multiply vs by to make sure they're not counted 
-        self.ms = np.ones(size)
-        
+        # turn off annihilated dislocations' contributions to various computations by
+        # multiplying them by 0 elementwise (if it does, contribute, multiply this by one)
+        self.mults = np.ones(size)
+
         # time at which the ith dislocation was annihilated. if i == 0 (TODO: nan), not eliminated   
         self.elim_times = np.zeros(size)
-        # self.elim_times = np.full(size, np.nan)
+
+
+@app.class_definition
+# this class represents a lattice cell with dislocations 
+class LatticeCell:
+
+    def __init__(self, N, sigma_ext, ye, L, t0, b, nu=0.3, rng_seed=None):
+        self.rng = np.random.default_rng(rng_seed)
+
+        self.x0s = self.rng.random(N) * L 
+        self.ys = self.rng.random(N) * L 
+
+
+        self.b = b 
+        self.bs = b * self.rng.permuted(
+            np.concatenate((
+                np.ones(int(N/2)),
+                 -(np.ones(int(N/2)))
+            ))
+        )
+
+        self.N = N 
+        self.nu = nu
+        self.sigma_ext = sigma_ext
+        self.ye = ye
+        self.L = L 
+        self.t0 = t0
+        self.LATTICE_EDGE_CT = int(L/b)
+
+
+    def apply_dislocations(self, dislocations_xs=None, dislocations_ys=None, bs=None):
+        if dislocations_xs is None:
+            dislocations_xs = self.x0s
+        if dislocations_ys is None:
+            dislocations_ys = self.ys
+        if bs is None:
+            bs = self.bs 
+
+        # non-dislocated lattice 
+        xs = np.linspace(0, self.L, self.LATTICE_EDGE_CT)
+        ys = np.linspace(0, self.L, self.LATTICE_EDGE_CT)
+        X_perfect, Y_perfect = np.meshgrid(xs, ys)
+        X_flat, Y_flat = X_perfect.flatten(), Y_perfect.flatten()
+
+        # iteratively apply dislocations to lattice points 
+        for c_x, c_y, b in np.column_stack((dislocations_xs, dislocations_ys, bs)):
+            u_x, u_y = self.calculate_displacements(X_flat, Y_flat, c_x, c_y, b)
+            X_flat = (X_flat + u_x) % self.L
+            Y_flat = (Y_flat + u_y) % self.L
+
+        self.lattice_points = np.column_stack((X_flat, Y_flat))
+        return X_flat, Y_flat
+
+
+    def calculate_displacements(self, X, Y, disloc_x, disloc_y, b):
+        # not a constant because it depends on whether b is pos or negative
+        b2pi = b / (2 * pi)
+
+        # existing lattice points
+        X_rel = X - disloc_x
+        Y_rel = Y - disloc_y
+        # in polar 
+        r = np.sqrt(X_rel**2 + Y_rel**2)
+        theta = np.arctan2(Y_rel, X_rel)
+
+        # horizontal and vertical displacement 
+        u_x = b2pi * (theta + (np.sin(2*theta) / (4*(1-self.nu))))
+        # the additive constant np.log(r + 1e-19) is to prevent log(0) errors 
+        u_y = -b2pi * ( ((1-2*self.nu)/(2*(1-self.nu))) * np.log(r) + 
+                        (np.cos(2*theta) / (4*(1-self.nu))) ) 
+        return u_x, u_y
+
+
+    def plot_lattice(self, 
+                     lattice_x, 
+                     lattice_y, 
+                     dislocations_xs, 
+                     dislocations_ys=None, 
+                     dislocations_bs=None):
+        if dislocations_ys is None:
+            dislocations_ys = self.ys
+        if dislocations_bs is None:
+            dislocations_bs = self.bs 
+
+        fig, ax = plt.subplots(figsize=(9, 9))
+
+        ax.scatter(lattice_x, lattice_y)
+
+        ax.scatter(dislocations_xs, dislocations_ys, c=dislocations_bs, label="dislocation centers")
+
+        for i, xy in enumerate(zip(dislocations_xs, dislocations_ys)):
+            ax.annotate(i, xy, c='r')
+
+        ax.axis('equal')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        return fig
+
+
+    def vs(self, sigma_fn, xs, ys=None, bs=None, L=None, sigma_ext=0):
+
+        if ys is None:
+            ys = self.ys
+        if bs is None:
+            bs = self.bs 
+        if L is None:
+            L = self.L
+
+        sigmas = sigma_fn(xs, ys, L)
+        assert np.all(np.isclose(0, np.diagonal(sigmas))), ("make sure your sigma_fn returns a "
+                                                            "matrix with zeros on the main diagonal")
+
+        return np.sign(bs) * (np.sign(bs) @ sigmas + sigma_ext)
+
+
+    def relax_ivp(self, num_tpoints=1000):
+        self.elim_relax = Eliminator(self.N)
+        ivp_result = solve_ivp(dxsdt_func,
+                               (self.t0, num_tpoints*self.t0), 
+                               self.x0s, 
+                               t_eval=np.linspace(self.t0, num_tpoints*self.t0, num_tpoints),
+                               method="RK45",
+                               args=(0, self.elim_relax, self)
+                              )
+        return ivp_result
 
 
 @app.cell
-def _(dxsdt_func, t0):
-    # this class represents a lattice cell with dislocations 
-    class LatticeCell:
-
-        def __init__(self, N, sigma_ext, ye, L, t0, b, nu=0.3, rng_seed=None):
-            self.rng = np.random.default_rng(rng_seed)
-
-            self.x0s = self.rng.random(N) * L 
-            self.ys = self.rng.random(N) * L 
-
-
-            self.b = b 
-            self.bs = b * self.rng.permuted(
-                np.concatenate((
-                    np.ones(int(N/2)),
-                     -(np.ones(int(N/2)))
-                ))
-            )
-
-            self.nu = nu
-            self.sigma_ext = sigma_ext
-            self.ye = ye
-            self.L = L 
-            self.t0 = t0
-            self.LATTICE_EDGE_CT = int(L/b)
-
-
-        def apply_dislocations(self, dislocations_xs=None, dislocations_ys=None, bs=None):
-            if dislocations_xs is None:
-                dislocations_xs = self.x0s
-            if dislocations_ys is None:
-                dislocations_ys = self.ys
-            if bs is None:
-                bs = self.bs 
-
-            # non-dislocated lattice 
-            xs = np.linspace(0, self.L, self.LATTICE_EDGE_CT)
-            ys = np.linspace(0, self.L, self.LATTICE_EDGE_CT)
-            X_perfect, Y_perfect = np.meshgrid(xs, ys)
-            X_flat, Y_flat = X_perfect.flatten(), Y_perfect.flatten()
-
-            # iteratively apply dislocations to lattice points 
-            for c_x, c_y, b in np.column_stack((dislocations_xs, dislocations_ys, bs)):
-                u_x, u_y = self.calculate_displacements(X_flat, Y_flat, c_x, c_y, b)
-                X_flat = (X_flat + u_x) % self.L
-                Y_flat = (Y_flat + u_y) % self.L
-
-            self.lattice_points = np.column_stack((X_flat, Y_flat))
-            return X_flat, Y_flat
-
-
-        def calculate_displacements(self, X, Y, disloc_x, disloc_y, b):
-            # not a constant because it depends on whether b is pos or negative
-            b2pi = b / (2 * pi)
-
-            # existing lattice points
-            X_rel = X - disloc_x
-            Y_rel = Y - disloc_y
-            # in polar 
-            r = np.sqrt(X_rel**2 + Y_rel**2)
-            theta = np.arctan2(Y_rel, X_rel)
-
-            # horizontal and vertical displacement 
-            u_x = b2pi * (theta + (np.sin(2*theta) / (4*(1-self.nu))))
-            # the additive constant np.log(r + 1e-19) is to prevent log(0) errors 
-            u_y = -b2pi * ( ((1-2*self.nu)/(2*(1-self.nu))) * np.log(r) + 
-                            (np.cos(2*theta) / (4*(1-self.nu))) ) 
-            return u_x, u_y
-
-
-        def plot_lattice(self, 
-                         lattice_x, 
-                         lattice_y, 
-                         dislocations_xs, 
-                         dislocations_ys=None, 
-                         dislocations_bs=None):
-            if dislocations_ys is None:
-                dislocations_ys = self.ys
-            if dislocations_bs is None:
-                dislocations_bs = self.bs 
-
-            fig, ax = plt.subplots(figsize=(9, 9))
-
-            ax.scatter(lattice_x, lattice_y)
-
-            ax.scatter(dislocations_xs, dislocations_ys, c=dislocations_bs, label="dislocation centers")
-
-            for i, xy in enumerate(zip(dislocations_xs, dislocations_ys)):
-                ax.annotate(i, xy, c='r')
-
-            ax.axis('equal')
-            ax.set_xlabel('x')
-            ax.set_ylabel('y')
-            return fig
-
-
-        def vs(self, sigma_fn, xs, ys=None, bs=None, L=None, sigma_ext=0):
-
-            if ys is None:
-                ys = self.ys
-            if bs is None:
-                bs = self.bs 
-            if L is None:
-                L = self.L
-
-            sigmas = sigma_fn(xs, ys, L)
-            assert np.all(np.isclose(0, np.diagonal(sigmas))), ("make sure your sigma_fn returns a "
-                                                                "matrix with zeros on the main diagonal")
-
-            return np.sign(bs) * (np.sign(bs) @ sigmas + sigma_ext)
-
-    
-        def relax_ivp(self, num_tpoints=1000):
-            self.elim_relax = Eliminator(self.N)
-            ivp_result = solve_ivp(dxsdt_func,
-                                   (t0, num_tpoints*t0), 
-                                   self.x0s, 
-                                   t_eval=np.linspace(t0, num_tpoints*t0, num_tpoints),
-                                   method="RK45",
-                                   args=(0, self.elim_relax, self)
-                                  )
-            return ivp_result
-        
-    return (LatticeCell,)
-
-
-@app.cell
-def _(LatticeCell):
-    thing = LatticeCell(40, 0, 1, 100, 1, 1, rng_seed=1)
+def _():
+    thing = LatticeCell(20, 0, 1, 100, 1, 1, rng_seed=1)
     X_thing, Y_thing = thing.apply_dislocations()
     mo.mpl.interactive(thing.plot_lattice(X_thing, Y_thing, dislocations_xs=thing.x0s))
     return (thing,)
@@ -594,6 +538,25 @@ def _(LatticeCell):
 @app.cell
 def _(thing):
     print(thing.vs(sigma_s_analytic_sum, thing.x0s))
+    return
+
+
+@app.cell
+def _(thing):
+    res = thing.relax_ivp()
+    return (res,)
+
+
+@app.cell
+def _(res, thing):
+    print(res.y[:,-1])
+    print(thing.elim_relax.mults)
+    print(thing.elim_relax.elim_times)
+    return
+
+
+@app.cell
+def _():
     return
 
 
